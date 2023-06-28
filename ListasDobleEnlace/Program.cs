06fb5e2d0209d9e8827d328bdf375fb8dafc0485
Edgar Renato Pastrana Alvarez <edgarpastrana95@gmail.com>
2023-06-27 17:04:26 -0700

    Correción de program.cs

--- ListasDobleEnlace/Program.cs
+++ ListasDobleEnlace/Program.cs
@@ -1,22 +1,21 @@
-﻿namespace ListaDobleEnlace
+﻿namespace ListasDobleEnlace
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Lista 1 = new Lista();
-            1.insertarNodo();
-            1.insertarNodo();
-            1.insertarNodo();
-            1.insertarNodo();
-            1.insertarNodo();
+            Lista Lista1 = new Lista();
+            Lista1.InsertarNodo();
+            Lista1.InsertarNodo();
+            Lista1.InsertarNodo();
+            Lista1.InsertarNodo();
+            Lista1.InsertarNodo();
             Console.WriteLine("\n PRIMERO AL ULTIMO");
-            1.desplegarListaPU();
+            Lista1.desplegarListaPU();
             Console.WriteLine("\n ULTIMO AL PRIMERO");
-            1.desplegarListaUP();
+            Lista1.desplegarListaUP();
             Console.WriteLine();
 
         }
     }
 }
-
